acb4b238894da6ea724d8486446f52e64b4e184f
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-10-09 19:22:19 +0530

    Use system time instead of database time for OTP validation

--- core-services/egov-otp/src/main/java/org/egov/persistence/repository/TokenRepository.java
+++ core-services/egov-otp/src/main/java/org/egov/persistence/repository/TokenRepository.java
@@ -22,10 +22,10 @@ public class TokenRepository {
     private static final int UPDATED_ROWS_COUNT = 1;
     private static final String NO = "N";
     private static final String INSERT_TOKEN = "insert into eg_token(id,tenantid,tokennumber,tokenidentity,validated,ttlsecs,createddate,createdby,version,createddatenew) values (:id,:tenantId,:tokenNumber,:tokenIdentity,:validated,:ttlSecs,:createdDate,:createdBy,:version,:createddatenew);";
-    private static final String GETTOKENS_BY_NUMBER_IDENTITY_TENANT = "select * from eg_token where tokenidentity=:tokenIdentity and tenantid=:tenantId and ((extract(epoch from now()) * 1000 - createddatenew)/1000)::int <= ttlsecs and validated = 'N'";
+    private static final String GETTOKENS_BY_NUMBER_IDENTITY_TENANT = "select * from eg_token where tokenidentity=:tokenIdentity and tenantid=:tenantId and (:currenttime - createddatenew)/1000)::int <= ttlsecs and validated = 'N'";
     private static final String UPDATE_TOKEN = "update eg_token set validated = 'Y' where id = :id";
     private static final String GETTOKEN_BYID = "select * from eg_token where id=:id";
-    private static final String UPDATETOKEN_TLL_BYID = "update eg_token set ttlsecs = (extract (epoch from now()) - createddatenew / 1000)::int + :ttl where id = :id";
+    private static final String UPDATETOKEN_TLL_BYID = "update eg_token set ttlsecs = (:currenttime - createddatenew / 1000)::int + :ttl where id = :id";
 
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
@@ -77,6 +77,7 @@ public class TokenRepository {
         final Map<String, Object> tokenInputs = new HashMap<String, Object>();
         tokenInputs.put("tokenIdentity", request.getIdentity());
         tokenInputs.put("tenantId", request.getTenantId());
+        tokenInputs.put("currenttime", System.currentTimeMillis());
         List<Token> domainTokens = namedParameterJdbcTemplate.query(GETTOKENS_BY_NUMBER_IDENTITY_TENANT, tokenInputs,
                 new TokenRowMapper());
         return new Tokens(domainTokens);
@@ -99,6 +100,7 @@ public class TokenRepository {
         final Map<String, Object> tokenInputs = new HashMap<String, Object>();
         tokenInputs.put("id", t.getUuid());
         tokenInputs.put("ttl", otpConfiguration.getTtl());
+        tokenInputs.put("currenttime", System.currentTimeMillis());
         return namedParameterJdbcTemplate.update(UPDATETOKEN_TLL_BYID, tokenInputs);
     }
 }
