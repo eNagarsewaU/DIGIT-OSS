96f0c515e2698109cc1166b1014bbd9f12444ac4
vinothrallapalli-egov <vinoth.r@egovernments.org>
2022-10-18 12:50:28 +0530

    Added loggers to debug

--- municipal-services/tl-services/src/main/java/org/egov/tl/workflow/WorkflowIntegrator.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/workflow/WorkflowIntegrator.java
@@ -138,6 +138,7 @@ public class WorkflowIntegrator {
 			JSONObject workFlowRequest = new JSONObject();
 			workFlowRequest.put(REQUESTINFOKEY, tradeLicenseRequest.getRequestInfo());
 			workFlowRequest.put(WORKFLOWREQUESTARRAYKEY, array);
+			log.info("******Workflow Payload*****"+workFlowRequest.toString());
 			String response = null;
 			try {
 				response = rest.postForObject(config.getWfHost().concat(config.getWfTransitionPath()), workFlowRequest, String.class);
