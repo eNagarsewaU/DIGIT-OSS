19d5e43f33742fa70a63651ac10bd7ca98d09db1
palak-egov <86659286+palak-egov@users.noreply.github.com>
2022-11-17 13:03:53 +0530

    Update PropertyService.java

--- municipal-services/property-services/src/main/java/org/egov/pt/service/PropertyService.java
+++ municipal-services/property-services/src/main/java/org/egov/pt/service/PropertyService.java
@@ -498,7 +498,7 @@ public class PropertyService {
     	log.info("~~~~~~~~~~ Current state = "+currentState);
     	Property property = propertyRequest.getProperty();
 		BigDecimal balanceAmount = util.getBalanceAmount(propertyRequest);
-		if ((balanceAmount.compareTo(BigDecimal.ZERO) <= 0)) {
+		if ((balanceAmount.compareTo(BigDecimal.ZERO) < 0)) {
 			log.info("~~~~~~~~~~ Enabling skip payment for balance = "+balanceAmount);
 			String action = EMPTY;
 			if("APPLICATION_FEE_PAYMENT".equalsIgnoreCase(currentState))
