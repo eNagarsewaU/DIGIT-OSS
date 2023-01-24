e32d6e98d086b03208612f8833ccfbb636ddca0b
palak-egov <86659286+palak-egov@users.noreply.github.com>
2023-01-24 15:47:54 +0530

    Update PaymentValidator.java

--- business-services/collection-services/src/main/java/org/egov/collection/util/PaymentValidator.java
+++ business-services/collection-services/src/main/java/org/egov/collection/util/PaymentValidator.java
@@ -498,7 +498,7 @@ public class PaymentValidator {
 
 			String response = serviceRequestRepository
 					.fetchGetResult(applicationProperties.getRazorPayUrl() + paymentRequest.getPayment().getIfscCode());
-			ObjectNode objectNode = paymentRequest.getPayment().getAdditionalDetails().deepCopy();
+			ObjectNode objectNode = (ObjectNode) paymentRequest.getPayment().getAdditionalDetails().deepCopy();
 			if (objectNode == null) {
 				ObjectMapper mapper = new ObjectMapper();
 				objectNode = mapper.createObjectNode();
